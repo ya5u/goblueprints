a95c0a224e942fb41e43af75da376f2b6afff18b
Yasutaka Shinzaki <y.lax.shinzaki43@gmail.com>
2018-05-29 00:10:28 +0900

    done by 1.3

---
+++ chat/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// templは1つのテンプレートを表します。
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します。
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します。
+
+	r := newRoom()
+
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// チャットルームを開始します。
+	go r.run()
+
+	// Webサーバーを起動します。
+	log.Println("Webサーバーを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
