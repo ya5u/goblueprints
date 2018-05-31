df64dab3de8cb5d17234a6aedd263dac020d273b
Yasutaka Shinzaki <y.lax.shinzaki43@gmail.com>
2018-06-01 05:37:49 +0900

    done 3.2.1.1

---
+++ chat/avatar.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+)
+
+// ErrNoAvatarURLはAvatarインスタンスがアバターのURLを返すことができない場合に発生するエラーです。
+var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません。")
+
+// Avatarはユーザーのプロフィール画像を表す型です。
+type Avatar interface {
+	// GetAvatarURLは指定されたクライアントのアバターのURLを返します。
+	// 問題が発生した場合にはエラーを返します。特に、URLを取得できなかった場合にはErrNoAvatarURLを返します。
+	GetAvatarURL(c *client) (string, error)
+}
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
