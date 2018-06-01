8106299105de1e2f96d7715f1314a39a48e3fc54
Yasutaka Shinzaki <y.lax.shinzaki43@gmail.com>
2018-06-01 19:07:01 +0900

    done 3.2.1.3

--- chat/avatar.go
+++ chat/avatar.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // ErrNoAvatarURLはAvatarインスタンスがアバターのURLを返すことができない場合に発生するエラーです。
@@ -13,6 +17,7 @@ type Avatar interface {
 	// 問題が発生した場合にはエラーを返します。特に、URLを取得できなかった場合にはErrNoAvatarURLを返します。
 	GetAvatarURL(c *client) (string, error)
 }
+
 type AuthAvatar struct{}
 
 var UseAuthAvatar AuthAvatar
@@ -21,6 +26,21 @@ func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlStr, ok := url.(string); ok {
 			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
+type GravatarAvatar struct{}
+
+var UseGravatar GravatarAvatar
+
+func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailStr, ok := email.(string); ok {
+			m := md5.New()
+			io.WriteString(m, strings.ToLower(emailStr))
+			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
 		}
 	}
 	return "", ErrNoAvatarURL
